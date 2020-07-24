9d0839baa17810f49e85540359480d76ed699eca
Akshit Arora <akshitarora9211@gmail.com>
2020-07-24 17:53:17 +0530

    onclick trailer feature added

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ function App() {
       />
       <Row title="Trending" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      {/*   <Row title="Action" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} /> */}
     </div>
   );
 }
--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "../css/Row.css";
 
 const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, bigPoster }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -15,12 +18,36 @@ function Row({ title, fetchUrl, bigPoster }) {
     }
     fetchData();
   }, [fetchUrl]);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams.get("v"));
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div className="row">
       <h2 className="row-title">{title}</h2>
       <div className="row-posters">
         {movies.map((movie) => (
           <img
+            onClick={() => {
+              handleClick(movie);
+            }}
             key={movie.id}
             className={`row-poster ${bigPoster && "row-bigPoster"}`}
             src={`${BASE_IMAGE_URL}${
@@ -30,6 +57,7 @@ function Row({ title, fetchUrl, bigPoster }) {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
